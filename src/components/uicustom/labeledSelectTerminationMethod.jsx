4db5f11a11ec03127150038525925dbe35c37093
Christian Lang <christian.lang92@gmx.de>
2024-05-22 21:26:10 +0200

    changed termination method names

--- src/components/uicustom/labeledSelectTerminationMethod.jsx
+++ src/components/uicustom/labeledSelectTerminationMethod.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import LabeledSelect from './labeledSelect';
+import * as Constants from "@/components/utility/constants";
 
 const options = [
-    { value: 'first', label: 'first return' },
-    { value: 'second', label: 'second return' },
-    { value: 'time', label: 'advection time' }
+    { value: Constants.TERMINATION_METHOD_FIRST_RETURN, label: 'first return' },
+    { value: Constants.TERMINATION_METHOD_SECOND_RETURN, label: 'second return' },
+    { value: Constants.TERMINATION_METHOD_UNLIMITED, label: 'no return limit' }
 ];
 
 const LabeledSelectTerminationMethod = () => (
